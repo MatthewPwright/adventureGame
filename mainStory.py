2c1ddde0777e711925d52ffd64a00d576b110a7f
MatthewPwright <87259476+MatthewPwright@users.noreply.github.com>
2021-07-14 22:06:13 -0400

    Add files via upload

---
+++ mainStory.py
@@ -0,0 +1,85 @@
+import random
+import combat 
+import Damage
+import time
+import sys
+
+#What I want to do here is develope a choose you're own adventure game
+#D&D rpg style with an inventory system and magic items
+
+        #Damage
+            #going to be a random chance for the damage delt by each respective weapon
+            #Fire damage, wizard, lights the enemy on fire causing damage every turn
+
+    #Combat 
+        #Turn based system player always starts
+            #player turn options of using an item, fighting, or running
+
+
+
+
+
+
+
+
+#Open with a scene in a tavern, A wizard makes a image appear on the table then disappear and reapear floating in mid air
+def main
+    print("You are in a the only tavern in town enjoying the night. ")
+#People ordering drinks and listing to the bard sing in the corner
+    time.sleep(3):
+    print("Theres a wizard making a fireworks show ")
+#The fighter arm restling some poor soul in the middle of the room
+    time.sleep(3):
+    print("And fighter arm wresting in the middle of the wide room ")
+    time.sleep(3):
+    d1 = input ("Do you want to play as a wizard(A), fighter(B) ? ")
+    if d1 == A
+        print("You are a wizard, you've spent years of your life devoted to learning the arcane")
+            def wizard
+                print("A woman in black approaches you; she comes with a question. ")
+                time.sleep(3):
+                print("Would you like to go on a sick quest? ")
+                if d2 == C
+                print("")
+
+
+#Each of these are character options to play as in the game
+#Depending on who the player chooses is what skills they will start with in game
+
+
+#Welcome to blah blah tells the player about the different options to play as
+
+
+#If wizard is chosen, jump to wizard story line
+    
+
+#If bard is chosen, jump to bard story line
+
+
+
+#If fighter is chosen jump to fighter story line
+
+
+
+
+
+
+
+
+
+#Wizard story line
+
+
+
+
+
+
+
+#Bard story line
+
+
+
+
+
+
+#Fighter story line
